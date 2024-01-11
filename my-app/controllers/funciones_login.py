0f85b0ea715d24ac86a4a1a19194248f9c9614d9
Yoma1999 <karen.balladares.est@tecazuay.edu.ec>
2024-01-11 11:45:59 -0500

    1

--- my-app/controllers/funciones_login.py
+++ my-app/controllers/funciones_login.py
@@ -11,9 +11,9 @@ import re
 from werkzeug.security import generate_password_hash
 
 
-def recibeInsertRegisterUser(cedula, name, surname, id_area, id_rol, pass_user):
+def recibeInsertRegisterUser(cedula, name, surname, id_area, id_rol, pass_user, Telefono):
     respuestaValidar = validarDataRegisterLogin(
-        cedula, name, surname, pass_user)
+        cedula, name, surname, pass_user, Telefono)
 
     if (respuestaValidar):
         nueva_password = generate_password_hash(pass_user, method='scrypt')
@@ -21,8 +21,8 @@ def recibeInsertRegisterUser(cedula, name, surname, id_area, id_rol, pass_user):
             with connectionBD() as conexion_MySQLdb:
                 with conexion_MySQLdb.cursor(dictionary=True) as mycursor:
                     sql = """
-                    INSERT INTO usuarios(cedula, nombre_usuario, apellido_usuario, id_area, id_rol, password) 
-                    VALUES (%s, %s, %s, %s, %s, %s)
+                    INSERT INTO usuarios(cedula, nombre_usuario, apellido_usuario, id_area, id_rol, password, Telefono) 
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)
                     """
                     valores = (cedula, name, surname, id_area, id_rol, nueva_password)
                     mycursor.execute(sql, valores)
